d49c5013796cd57928a4c14021106b3f532e62e5
Korzhevykh-Veronika <55015773+Korzhevykh-Veronika@users.noreply.github.com>
2019-12-12 10:37:08 +0200

    FairyTale

---
+++ Homework_1/Homework_1/Animals/Animal.cs
@@ -0,0 +1,14 @@
+﻿namespace Homework_1
+{
+    internal abstract class Animal
+    {
+        public string Name { get; protected set; }
+        protected int Age;
+        protected double Height;
+
+        public virtual string Info()
+        {
+            return string.Format("{0} с возрастом {1} лет и ростом {2} метра ", Name, Age,Height);
+        }
+    }
+ }
