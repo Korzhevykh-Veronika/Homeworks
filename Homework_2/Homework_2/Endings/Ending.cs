b5d180e9864fe00809d33f9ce94283fdf777c742
Korzhevykh-Veronika <55015773+Korzhevykh-Veronika@users.noreply.github.com>
2019-12-15 22:05:59 +0200

    Homework_2

---
+++ Homework_2/Homework_2/Endings/Ending.cs
@@ -0,0 +1,10 @@
+﻿namespace Homework_1.Endings
+{
+    public class Ending
+    {
+        public virtual string GetEnding()
+        {
+            return "Вот и сказочке конец! ";
+        }
+    }
+}
---
+++ Homework_2/Homework_2/Endings/Negative.cs
@@ -0,0 +1,10 @@
+﻿namespace Homework_1.Endings
+{
+    class Negative : Ending
+    {
+        public override string GetEnding()
+        {
+            return base.GetEnding() + "Кто не слушал - вам конец.";
+        }
+    }
+}
