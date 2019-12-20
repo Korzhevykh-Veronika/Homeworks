c1d539d3d4201dec6804c00c78844cb90a342948
Korzhevykh-Veronika <55015773+Korzhevykh-Veronika@users.noreply.github.com>
2019-12-20 07:23:26 +0200

    Интерфейсы

---
+++ Homework_2/Homework_1/Endings/Positive.cs
@@ -0,0 +1,10 @@
+﻿namespace Homework_1.Endings
+{
+    class Positive : Ending
+    {
+        public override string GetEnding()
+        {
+            return base.GetEnding() + "А кто слушал - молодец";
+        }
+    }
+}
