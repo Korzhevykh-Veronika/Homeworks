74a9d172df0654058bf622e01066e7942e6f2d5b
Korzhevykh-Veronika <55015773+Korzhevykh-Veronika@users.noreply.github.com>
2019-12-20 07:18:54 +0200

    Делегаты, события

---
+++ Homework_3/Homework_1/Randomizer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Homework_1
+{
+    static class Randomizer
+    {
+        static Random _random = new Random();
+
+        public static object GetRandomElementFromArray(object[] array)
+        {
+            int mIndex = _random.Next(array.Length);
+            Thread.Sleep(1000);
+
+            return array[mIndex];
+        }
+
+        public static ConsoleColor GetRandomConsoleColor()
+        {
+            return (ConsoleColor)_random.Next(15);
+        }
+    }
+}
